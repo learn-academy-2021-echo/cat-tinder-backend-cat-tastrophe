02339039a85335e9932038e4e70ca216206529fd
Punjabi2021 <93889575+Punjabi2021@users.noreply.github.com>
2022-02-05 18:26:21 -0800

    worked on backend

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -1,7 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe "Cats", type: :request do
+RSpec.describe "Cat", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cats" do
+      Cat.create(
+        name: "Jack",
+        age: 5,
+        enjoys: "Furrrrociously hunting bugs.",
+        image: "https://images.unsplash.com/photo-1492370284958-c20b15c692d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1049&q=80"
+      )
+
+      # Make a request
+      get '/cats'
+
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
+    end
+  end
+end
+
+
+
+describe "POST /create" do
+  it "creates a cat" do
+    # The params we are going to send with the request
+    cat_params = {
+      cat: {
+        name: "Homer",
+        age: 12,
+        enjoys: "Food mostly, really just food.",
+        image: "https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80"
+      }
+    }
+
+    # Send the request to the server
+    post '/cats', params: cat_params
+
+    # Assure that we get a success back
+    expect(response).to have_http_status(200)
+
+    # Look up the cat we expect to be created in the db
+    cat = Cat.first
+
+    # Assure that the created cat has the correct attributes
+    expect(cat.name).to eq 'Homer'
   end
 end
