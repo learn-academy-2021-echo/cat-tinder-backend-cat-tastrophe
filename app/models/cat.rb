fff8e89e8e37399cb2f56d497c4f02e1358b3d32
Punjabi2021 <93889575+Punjabi2021@users.noreply.github.com>
2022-02-07 20:31:13 -0800

    finished api-validations edits

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,5 @@
 class Cat < ApplicationRecord
-  validates :name, :age , :enjoys ,:image, presence: true
-  validates :enjoys ,length: {minimum:10}
-  validates :age ,numericality: {minimum:1}
+  validates :name, :age, :enjoys, :image, presence: true
+  validates :enjoys, length: {minimum:10}
+  validates :age, numericality: {minimum:1}
 end
